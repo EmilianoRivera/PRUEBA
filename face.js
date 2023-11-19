dcf636b168e2ea47ca651661344a5a48f494b1fb
EmilianoRivera <jriveras2100@alumno.ipn.mx>
2023-11-18 23:10:40 -0600

    Organizado del codigo

---
+++ face.js
@@ -0,0 +1,56 @@
+
+function checkLoginState() {
+    FB.getLoginStatus(function (response) {
+        statusChangeCallback(response);
+    });
+}
+function statusChangeCallback(response) { 
+    console.log('statusChangeCallback');
+    console.log(response);                   
+    if (response.status === 'connected') {   
+        var accessToken = response.authResponse.accessToken;
+        var userId = response.authResponse.userID;
+
+        FB.api('/' + userId + '/accounts', 'GET', { access_token: accessToken }, function (response) {
+            if (response && !response.error) {
+                console.log(response);
+
+                for (var i = 0; i < response.data.length; i++) {
+                    var page = response.data[i];
+                    console.log('ID de la página: ' + page.id);
+                    console.log('Nombre de la página: ' + page.name);
+                }
+            }
+        });
+        console.log(accessToken)
+        console.log(userId)
+
+        testAPI();
+    } else {                                
+        document.getElementById('status').innerHTML = 'Please log ' +
+            'into this webpage.';
+    }
+}
+window.fbAsyncInit = function () {
+    FB.init({
+        appId: '1853633641760357',
+        cookie: true,                     
+        xfbml: true,                   
+        version: 'v18.0'          
+    });
+
+    // USAR METODO POST Y API TOKEN, PARAMETROS: TITLE, CONTENT 
+    FB.getLoginStatus(function (response) {   
+        statusChangeCallback(response);       
+    });
+checkLoginState();
+};
+
+function testAPI() {                     
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function (response) {
+        console.log('Successful login for: ' + response.name);
+        document.getElementById('status').innerHTML =
+            'Thanks for logging in, ' + response.name + '!';
+    });
+}
---
+++ firebase.js
@@ -0,0 +1,67 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+import { getFirestore, collection, getDocs, onSnapshot } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+
+    apiKey: "AIzaSyAozZm8FRSn9uMa6plxuL0PXlGYH2b9Sos",
+
+    authDomain: "prueba-e44fc.firebaseapp.com",
+
+    projectId: "prueba-e44fc",
+
+    storageBucket: "prueba-e44fc.appspot.com",
+
+    messagingSenderId: "439748782067",
+
+    appId: "1:439748782067:web:2eb066c3fb5cb3992a5486"
+
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const reportesContainer = document.getElementById("reportes-container");
+
+
+const onGetReportes = (callback) => {
+    const query = collection(db, 'reportes');
+
+    const unsubscribe = onSnapshot(query, (querySnapshot) => {
+        callback(querySnapshot);
+    });
+
+    // Devolvemos la función para desuscribirse cuando sea necesario
+    return unsubscribe;
+}
+
+window.addEventListener('DOMContentLoaded', async () => {
+    try {
+        const unsubscribe = onGetReportes((querySnapshot) => {
+            reportesContainer.innerHTML = "";  // Limpiamos el contenedor antes de agregar nuevos elementos
+            querySnapshot.forEach((doc) => {
+                const reporte = doc.data();
+                reportesContainer.innerHTML += `<div class="car card-body mt-2 border-primary"> 
+            <div>
+                <img src=" ${reporte.imagenURL}" length ="100" width="100">
+            </div>
+            <p>${reporte.ubicacion}</p>
+            <p>${reporte.descripcion}</p>
+        </div>`;
+            });
+        });
+
+        // Puedes usar `unsubscribe` para dejar de escuchar los cambios en algún momento
+        // Por ejemplo, si dejas la página o dejas de necesitar las actualizaciones en tiempo real
+        // unsubscribe();
+    } catch (error) {
+        console.error('Error al obtener reportes:', error);
+    }
+});
+
+
+
