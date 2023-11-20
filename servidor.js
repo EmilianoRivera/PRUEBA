4af9f066a892249ebb81168a590fa6c8dac28943
EmilianoRivera <jriveras2100@alumno.ipn.mx>
2023-11-19 22:28:56 -0600

    Quito el setInterval, y ya aparece la imagen

--- servidor.js
+++ servidor.js
@@ -13,11 +13,15 @@ app.listen(process.env.PORT || 3000, () => {
     console.log("Inicio el servidor")
 })
 const pageId = "121835611022693"
+const accountId = "103758917811285"
 const access_token = "EAAMTmmZCNPYkBOZB7Qc9kdndWCh7PYQMFF5CxMPYLnt4yVeqlIXghcdyYpc027ZCATeR54YgIvdMuhoAZBnaepuvyuXOM1GZAMjnPzRvT48a5xBulzLQFrzpIfH4PCIjUnXZC0mvmKbbZAz2ilncoqPKDlQSRY4xNBMPTIGZBXyS59HPRfI6BOQdGnaZBX6dF1B8ZD"
 async function realizarPublicacion(text, img) {
     try {
         const imageUrl = encodeURIComponent(img);
-        const response = await axios.post(`https://graph.facebook.com/v18.0/${pageId}/photos?url=${imageUrl}&message=${text}&access_token=${access_token}`, null);
+        const response = await 
+        axios.post(
+        `https://graph.facebook.com/v18.0/${pageId}/photos?url=${imageUrl}&message=${text}&tags=${accountId}&access_token=${access_token}`,
+         null);
 
         console.log(response.data);
         return response.data;
@@ -26,7 +30,22 @@ async function realizarPublicacion(text, img) {
         throw error;  // Relanza el error para que pueda ser capturado por la promesa
     }
 }
+cargarReportes()
+  .then(async () => {
+    // Ahora puedes obtener datosPost con la información actualizada
+    const mensajes = datosPost.ubi.map((ubi, index) => {
+      const mensaje = `Bache reportado en: ${ubi} Descripción: ${datosPost.desc[index]}.  `;
+      const Img = `${datosPost.imgURL[index]}`; //no añadir nada de texto aca, sino se jode todo xd
+      realizarPublicacion(mensaje, Img);
+    });
+  })
+  .catch((error) => {
+    console.error('Error al cargar los reportes:', error);
+  });
 
+
+
+  /*
 cargarReportes()
 .then(() => {
     // Luego, configurar un temporizador para ejecutar la función cada 5 segundos
@@ -34,12 +53,12 @@ cargarReportes()
         await cargarReportes();
         // Ahora puedes obtener datosPost con la información actualizada
         const mensajes = datosPost.ubi.map((ubi, index) => {
-            const mensaje = `Bache reportado en: ${ubi} Descripción: ${datosPost.desc[index]}`;
-            const Img = `${datosPost.imgURL[index]}`;
+            const mensaje = `Bache reportado en: ${ubi} Descripción: ${datosPost.desc[index]}. @Soy de Azcapotzalco(CDMX) `;
+            const Img = `${datosPost.imgURL[index]}`; //no añadir nada de texto aca, sino se jode todo xd
             realizarPublicacion(mensaje, Img);
         });
     }, 6000); // 3600000 milisegundos = 1 hora
 })
 .catch((error) => {
     console.error('Error al cargar los reportes:', error);
-});
+});*/
