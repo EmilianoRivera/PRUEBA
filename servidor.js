16f6ce729495afd82051dc779970ba32841850d2
EmilianoRivera <jriveras2100@alumno.ipn.mx>
2023-11-19 19:23:02 -0600

    se postea la imagen

--- servidor.js
+++ servidor.js
@@ -17,7 +17,8 @@ const access_token = "EAAMTmmZCNPYkBOZB7Qc9kdndWCh7PYQMFF5CxMPYLnt4yVeqlIXghcdyY
 async function realizarPublicacion(text, img) {
     try {
         const imageUrl = encodeURIComponent(img);
-        const response = await axios.post(`https://graph.facebook.com/v18.0/${pageId}/feed?message=${text} ${imageUrl}&access_token=${access_token}`, null);
+        const response = await axios.post(`https://graph.facebook.com/v18.0/${pageId}/photos?url=${imageUrl}&message=${text}&access_token=${access_token}`, null);
+
         console.log(response.data);
         return response.data;
     } catch (error) {
@@ -27,15 +28,18 @@ async function realizarPublicacion(text, img) {
 }
 
 cargarReportes()
-    .then(() => {
+.then(() => {
+    // Luego, configurar un temporizador para ejecutar la función cada 5 segundos
+    setInterval(async () => {
+        await cargarReportes();
         // Ahora puedes obtener datosPost con la información actualizada
         const mensajes = datosPost.ubi.map((ubi, index) => {
             const mensaje = `Bache reportado en: ${ubi} Descripción: ${datosPost.desc[index]}`;
-            const Img = `Evidencia: ${datosPost.imgURL[index]}`;
+            const Img = `${datosPost.imgURL[index]}`;
             realizarPublicacion(mensaje, Img);
         });
-    })
-    .catch((error) => {
-        console.error('Error al cargar los reportes:', error);
-    });
-
+    }, 6000); // 3600000 milisegundos = 1 hora
+})
+.catch((error) => {
+    console.error('Error al cargar los reportes:', error);
+});
